b99f60eca719390165f496a1cf71dcaa8a35636f
GIOhnny <giohnny@gmail.com>
2023-05-23 22:36:24 +0300

    341 API

--- FruitsProject_mangoose/app.js
+++ FruitsProject_mangoose/app.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB")
 
 const fruitSchema = new mongoose.Schema({
   name: {type: String, required: [true,"The fruit must have a name!" ]} ,
